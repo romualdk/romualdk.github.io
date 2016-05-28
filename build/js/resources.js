b6c1ce4a3fb81f59e23ca4a5aea9b37b1576e8e2
romualdk <romuald.kowalczyk@gmail.com>
2016-05-28 12:52:35 +0200

    new build

---
+++ build/js/resources.js
@@ -0,0 +1,127 @@
+game.resources = [
+    {
+        "name": "ItemPickUp16",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "Mister-Snarkypants",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "Telemetry_13",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "coin",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "explosion",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "fire",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "fire2",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "hit",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "jump",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "squeaky_closet_door_open_001",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "bat",
+        "type": "image",
+        "src": "data/img/bat.png"
+    },
+    {
+        "name": "bullet-0",
+        "type": "image",
+        "src": "data/img/bullet-0.png"
+    },
+    {
+        "name": "explosions",
+        "type": "image",
+        "src": "data/img/explosions.png"
+    },
+    {
+        "name": "font-nes_std_16",
+        "type": "image",
+        "src": "data/img/font-nes_std_16.png"
+    },
+    {
+        "name": "font-nes_std_8",
+        "type": "image",
+        "src": "data/img/font-nes_std_8.png"
+    },
+    {
+        "name": "frog",
+        "type": "image",
+        "src": "data/img/frog.png"
+    },
+    {
+        "name": "ghost",
+        "type": "image",
+        "src": "data/img/ghost.png"
+    },
+    {
+        "name": "items",
+        "type": "image",
+        "src": "data/img/items.png"
+    },
+    {
+        "name": "landing-smoke",
+        "type": "image",
+        "src": "data/img/landing-smoke.png"
+    },
+    {
+        "name": "players",
+        "type": "image",
+        "src": "data/img/players.png"
+    },
+    {
+        "name": "shoot-smoke",
+        "type": "image",
+        "src": "data/img/shoot-smoke.png"
+    },
+    {
+        "name": "skeleton",
+        "type": "image",
+        "src": "data/img/skeleton.png"
+    },
+    {
+        "name": "tileset-dungeon",
+        "type": "image",
+        "src": "data/img/tileset-dungeon.png"
+    },
+    {
+        "name": "title",
+        "type": "image",
+        "src": "data/img/title.png"
+    },
+    {
+        "name": "dungeon01",
+        "type": "tmx",
+        "src": "data/map/dungeon01.tmx"
+    }
+];
