f02330c1ed759f893e6e23cfde2bf544f00d314b
romualdk <romuald.kowalczyk@gmail.com>
2017-10-28 23:29:09 +0200

    test

--- reftest/js/referee.js
+++ reftest/js/referee.js
@@ -1,13 +1,94 @@
+var matchData = {
+    date: "",
+    team1: {
+        name: "",
+        points: 0
+    },
+    team2: {
+        name: "",
+        points: 0
+    },
+    events: []
+};
+
 var timerTime = 0;
+var renderedTime = "00:00";
 var interval = null;
 var isRunning = false;
 
+
+function getActualTime() {
+    var d = new Date();
+    var hours = d.getHours();
+    var minutes = d.getMinutes();
+    var seconds = d.getSeconds();
+
+    minutes = minutes >= 10 ? minutes : '0' + minutes;
+    seconds = seconds >= 10 ? seconds : '0' + seconds;
+
+    return hours + ":" + minutes + ":" + seconds;
+}
+
+function getTimerTime() {
+    /*const numOfMinutes = Math.floor(timerTime / 60),
+    numOfSeconds = timerTime % 60;
+
+    var minutes = numOfMinutes >= 10 ? numOfMinutes : '0' + numOfMinutes;
+    var seconds = numOfSeconds >= 10 ? numOfSeconds : '0' + numOfSeconds;
+
+    return minutes + ":" + seconds;*/
+
+    return renderedTime;
+}
+
+function addEvent(eventType, description) {
+    var event = {
+        type: eventType,
+        event: description,
+        timerTime: getTimerTime(),
+        actualTime: getActualTime()
+    };
+
+    matchData.events[matchData.events.length] = event;
+
+
+    $(".events").prepend('<div class="event"><div class="e_field_timer">' + event.timerTime + '</div><div class="e_field_description">' + event.event + ' (' + event.actualTime + ')</div></div>');
+}
+
+
+/*addEvent("normal", "Start");*/
+
+function addEventToList(event) {
+
+}
+
+
+
+
 $(".start").click(function() {
+    addEvent("normal", "Start");
     startTimer();
+    
 });;
 
 $(".stop").click(function() {
+    addEvent("normal", "Stop");
     stopTimer();
+    
+});
+
+$(".goal").click(function() {
+    if(isRunning) {
+        addEvent("goal", "Gol");
+    }
+    
+});
+
+$(".card").click(function() {
+    if(isRunning) {
+        addEvent("card", "Kartka");
+    }
+    
 });
 
 function startTimer() {
@@ -18,6 +99,9 @@ function startTimer() {
 
     $(".stop").removeClass("disabled");
     $(".start").addClass("disabled");
+
+    $(".goal").removeClass("inactive");
+    $(".card").removeClass("inactive");
 }
 
 // Stop timer
@@ -27,6 +111,9 @@ function stopTimer() {
 
     $(".start").removeClass("disabled");
     $(".stop").addClass("disabled");
+
+    $(".goal").addClass("inactive");
+    $(".card").addClass("inactive");
 }
 
 // Reset timer to zero
@@ -49,6 +136,8 @@ function incrementTimer() {
     var seconds = numOfSeconds >= 10 ? numOfSeconds : '0' + numOfSeconds;
     
     var time = minutes + ":" + seconds;
+
+    renderedTime = time;
 
     $('.timer').text(time);
 }
