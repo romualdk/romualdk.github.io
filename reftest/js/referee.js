44d26a5aaad8d55da24a38872eaba4f3fb0d3a24
romualdk <romuald.kowalczyk@gmail.com>
2017-10-29 11:00:09 +0100

    test

--- reftest/js/referee.js
+++ reftest/js/referee.js
@@ -1,144 +1,185 @@
-var matchData = {
-    date: "",
-    team1: {
-        name: "",
-        points: 0
-    },
-    team2: {
-        name: "",
-        points: 0
-    },
-    events: []
-};
-
-var timerTime = 0;
-var renderedTime = "00:00";
-var interval = null;
-var isRunning = false;
-
-
-function getActualTime() {
-    var d = new Date();
-    var hours = d.getHours();
-    var minutes = d.getMinutes();
-    var seconds = d.getSeconds();
-
-    minutes = minutes >= 10 ? minutes : '0' + minutes;
-    seconds = seconds >= 10 ? seconds : '0' + seconds;
-
-    return hours + ":" + minutes + ":" + seconds;
+MatchTimer = {};
+MatchTimer.data = {
+    
 }
 
-function getTimerTime() {
-    /*const numOfMinutes = Math.floor(timerTime / 60),
-    numOfSeconds = timerTime % 60;
+MatchTimer.reset = function() {
+    this.interval = null;
+    this.data = {
+        date: null,
+        timerTime: 0,
+        renderedTime: null,
+        isRunning: false,
+        team1: {
+            name: "FS PROSKOWO",
+            points: 0
+        },
+        team2: {
+            name: "Lyon",
+            points: 0
+        },
+        events: []
+    };
+
+    
+
+    // load state
+    this.initData();
+    console.log(this.data);
+
+    this.setRenderedTime();
+    this.refreshTimerView();
+    this.initEventsList();
+
+    // init
+    if(this.data.isRunning) {
+        this.setStateRunning();
+    }
+    else {
+        this.setStatePaused();
+    }
+
+
+
+    $(".start").click(function() {
+        MatchTimer.addEvent("normal", "Start");
+        MatchTimer.setStateRunning();
+        
+    });;
+    
+    $(".stop").click(function() {
+        MatchTimer.addEvent("normal", "Pause");
+        MatchTimer.setStatePaused();
+        
+    });
+    
+    $(".goal").click(function() {
+        if(MatchTimer.data.isRunning) {
+            MatchTimer.addEvent("goal", "Gol");
+        }
+    });
+    
+    $(".card").click(function() {
+        if(MatchTimer.data.isRunning) {
+            MatchTimer.addEvent("card", "Kartka");
+        }
+    });
+}
+
+MatchTimer.initData = function() {
+    var data = localStorage.getItem('match');
+    
+    if(data != null) {
+        this.data = JSON.parse(data);
+    }
+    else {
+        var d = new Date();
+        this.data.date = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate();
+    }
+
+    //initEventsList()
+}
+
+MatchTimer.storeData = function() {
+    localStorage.setItem('match', JSON.stringify(this.data));
+}
+
+
+MatchTimer.setRenderedTime = function() {
+    const numOfMinutes = Math.floor(this.data.timerTime / 60),
+    numOfSeconds = this.data.timerTime % 60;
 
     var minutes = numOfMinutes >= 10 ? numOfMinutes : '0' + numOfMinutes;
     var seconds = numOfSeconds >= 10 ? numOfSeconds : '0' + numOfSeconds;
 
-    return minutes + ":" + seconds;*/
-
-    return renderedTime;
+    this.data.renderedTime = minutes + ":" + seconds;
 }
 
-function addEvent(eventType, description) {
-    var event = {
-        type: eventType,
-        event: description,
-        timerTime: getTimerTime(),
-        actualTime: getActualTime()
-    };
-
-    matchData.events[matchData.events.length] = event;
-
-
-    $(".events").prepend('<div class="event"><div class="e_field_timer">' + event.timerTime + '</div><div class="e_field_description">' + event.event + ' (' + event.actualTime + ')</div></div>');
+MatchTimer.initTimer = function() {
+    
 }
 
-
-/*addEvent("normal", "Start");*/
-
-function addEventToList(event) {
-
-}
-
-
-
-
-$(".start").click(function() {
-    addEvent("normal", "Start");
-    startTimer();
-    
-});;
-
-$(".stop").click(function() {
-    addEvent("normal", "Stop");
-    stopTimer();
-    
-});
-
-$(".goal").click(function() {
-    if(isRunning) {
-        addEvent("goal", "Gol");
-    }
-    
-});
-
-$(".card").click(function() {
-    if(isRunning) {
-        addEvent("card", "Kartka");
-    }
-    
-});
-
-function startTimer() {
-    if (!isRunning) {
-        isRunning = true;
-        interval = setInterval(incrementTimer, 1000);
-    }
+MatchTimer.setStateRunning = function() {
+    this.data.isRunning = true;
+    clearInterval(this.interval);
+    this.interval = setInterval(MatchTimer.incrementTimer, 1000);
 
     $(".stop").removeClass("disabled");
     $(".start").addClass("disabled");
 
     $(".goal").removeClass("inactive");
     $(".card").removeClass("inactive");
+
+    this.storeData();
 }
 
-// Stop timer
-function stopTimer() {
-    isRunning = false;
-    clearInterval(interval);
+MatchTimer.setStatePaused = function() {
+    this.data.isRunning = false;
+    clearInterval(this.interval);
 
     $(".start").removeClass("disabled");
     $(".stop").addClass("disabled");
 
     $(".goal").addClass("inactive");
     $(".card").addClass("inactive");
+
+    this.storeData();
 }
 
-// Reset timer to zero
-function resetTimer() {
-    stopTimer();
-    resetUI();
-    timerTime = 0;
-    seconds.innerText = '00';
-    minutes.innerText = '00';
+MatchTimer.incrementTimer = function() {
+    MatchTimer.data.timerTime += 1;
+    MatchTimer.setRenderedTime();
+    MatchTimer.refreshTimerView();
+
+    MatchTimer.storeData();
 }
 
-// Increment timer
-function incrementTimer() {
-    const numOfMinutes = Math.floor(timerTime / 60),
-        numOfSeconds = timerTime % 60;
-
-    timerTime = timerTime + 1;
-    
-    var minutes = numOfMinutes >= 10 ? numOfMinutes : '0' + numOfMinutes;
-    var seconds = numOfSeconds >= 10 ? numOfSeconds : '0' + numOfSeconds;
-    
-    var time = minutes + ":" + seconds;
-
-    renderedTime = time;
-
-    $('.timer').text(time);
+MatchTimer.refreshTimerView = function() {
+    $('.timer').text(MatchTimer.data.renderedTime);
 }
 
+
+MatchTimer.getActualTime = function() {
+    var d = new Date();
+    var hours = d.getHours();
+    var minutes = d.getMinutes();
+
+    minutes = minutes >= 10 ? minutes : '0' + minutes;
+
+    return hours + ":" + minutes;
+}
+
+
+MatchTimer.addEvent = function(type, description) {
+    var event = {
+        type: type,
+        description: description,
+        timerTime: this.data.renderedTime,
+        actualTime: this.getActualTime()
+    };
+
+    this.data.events[this.data.events.length] = event;
+
+    MatchTimer.addEventToList(event);
+
+    this.storeData();
+}
+
+MatchTimer.clearEventsList = function() {
+    // $(".events").empty();
+ }
+ 
+ MatchTimer.addEventToList = function(event) {
+     $(".events").prepend('<div class="event"><div class="e_field_timer">' + event.timerTime + '</div><div class="e_field_description">' + event.description + ' (' + event.actualTime + ')</div></div>');
+ }
+ 
+ 
+ MatchTimer.initEventsList = function() {
+     this.clearEventsList();
+     for(var i in this.data.events) {
+         this.addEventToList(this.data.events[i]);
+     }
+ }
+
+
+MatchTimer.reset();
