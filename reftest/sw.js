1421e9210263d6453cfd02a9374fbfccd18cc388
romualdk <romuald.kowalczyk@gmail.com>
2017-11-01 19:30:53 +0100

    test

--- reftest/sw.js
+++ reftest/sw.js
@@ -6,7 +6,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
 
     // Open a cache
-    caches.open('v001').then(function(cache) {
+    caches.open('v002').then(function(cache) {
 
       // Define what we want to cache
       return cache.addAll([
