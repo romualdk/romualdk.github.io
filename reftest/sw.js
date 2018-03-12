ae1528986589f28beb6ce56bd055a070e1c02965
romualdk <romuald.kowalczyk@gmail.com>
2018-03-12 18:52:33 +0100

    test

--- reftest/sw.js
+++ reftest/sw.js
@@ -2,7 +2,7 @@
 importScripts('js/cache-polyfill.js');
 
 
-var version = '0.50180130';
+var version = '0.50180312';
 var cacheName = 'cache_' + version;
 var dataCacheName = 'cache_data_' + version;
 
