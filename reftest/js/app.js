fee995acd0313f7b555cd48d15be777b326c8c2e
romualdk <romuald.kowalczyk@gmail.com>
2018-06-20 20:29:22 +0200

    test

--- reftest/js/app.js
+++ reftest/js/app.js
@@ -1,6 +1,6 @@
 // Registering ServiceWorker
 if ( 'serviceWorker' in navigator ) {
-  navigator.serviceWorker.register( 'sw.js?v=2018062014' ).then(function(registration) {
+  navigator.serviceWorker.register( 'sw.js?v=2018062015' ).then(function(registration) {
 
     // Registration was successful
     console.log( 'ServiceWorker registration successful. Scope: ' + registration.scope )
--- reftest/sw.js
+++ reftest/sw.js
@@ -2,7 +2,7 @@
 importScripts('js/cache-polyfill.js');
 
 
-var version = '0.50180620014';
+var version = '0.50180620015';
 var cacheName = 'cache_' + version;
 var dataCacheName = 'cache_data_' + version;
 
